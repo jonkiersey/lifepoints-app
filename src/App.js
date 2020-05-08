91d4dafef4652318b770a794ac6de70887ddd205
Jon Kiersey <jon.kiersey@gmail.com>
2020-05-07 17:43:27 -0700

    Add basic form for lifepoints entry

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
+import Select from 'react-select';
 import './App.css';
+
+class LifePointForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      description: '',
+      category: null,
+      points: 1
+    };
+
+    this.handleChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  };
+
+  handleTextChange = (event) => {
+    console.log(event.target);
+    this.setState({ [event.target.name]: event.target.value});
+  };
+
+  handleSelectChange = (selectedOption) => {
+    console.log('handleSelectChange', this);
+    this.setState({ category: selectedOption });
+  };
+
+  handleSubmit = (event) => {
+    alert('Submitted' + JSON.stringify(this.state));
+    event.preventDefault();
+  };
+
+  render = () => {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label>
+            Name:
+            <input name='name' type='text' value={this.state.name} onChange={this.handleTextChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Description:
+            <input name='description' type='text' value={this.state.description} onChange={this.handleTextChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Category:
+            <Select 
+              onChange={this.handleSelectChange}
+              options={[
+                { value: 'EXERCISE', label: 'Exercise' },
+                { value: 'HOUSEHOLD', label: 'Household' },
+                { value: 'SOCIAL', label: 'Social' },
+                { value: 'PERSONAL_DEVELOPMENT', label: 'Personal Development' },
+                { value: 'RELATIONSHIP', label: 'Relationship' },
+              ]}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Points:
+            <input name='points' type='number' value={this.state.points} onChange={this.handleTextChange} />
+          </label>
+        </div>
+        <input type='submit' value='Submit' />
+      </form>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Record a new LifePoint
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LifePointForm />
       </header>
     </div>
   );
