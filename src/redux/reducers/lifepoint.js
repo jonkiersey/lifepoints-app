4f61274294489bfbbc8205d1fa6770054f9c3e38
Jon Kiersey <jon.kiersey@gmail.com>
2020-05-08 20:22:13 -0700

    Add evidence of 'What the hell?'

--- src/redux/reducers/lifepoint.js
+++ src/redux/reducers/lifepoint.js
@@ -4,12 +4,16 @@ const initialState = {
     lifepoint: {}
 };
 
+let i = 0
+
 export default (state = initialState, action) => {
     switch (action.type) {
         case SAVE_LIFEPOINT:
             console.log('save lifepoint', action.data);
-            return { ...state, lifepoint: action.data };
+            return { ...state, lifepoint: 'hello' };
         default:
-            return state;
+            console.log('default action');
+            i += 1;
+            return { ...state, default: i, lifepoint: 'goodbye' };
     }
 }
