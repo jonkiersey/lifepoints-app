c3e33026cb93d01b74aadf66a855851316d04f43
Jon Kiersey <jon.kiersey@gmail.com>
2020-08-04 17:59:14 -0700

    Add bad logo but it's not the default react logo so that's something

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,7 +2,8 @@ import React from 'react';
  
 const Landing = () => (
   <div>
-    <h1>Landing</h1>
+    <h1>where the points <i>do</i> matter</h1>
+    <p>After creating an account an signing in, head over to Home to get started</p>
   </div>
 );
  
